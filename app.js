e4f8267a08cbc650948442f9020b71ef5248a4c2
jasonellul <jason.j.ellul@gmail.com>
2019-09-12 09:45:57 -0400

    Force Pushed All Files

--- app.js
+++ app.js
@@ -20,15 +20,14 @@ io.on('connection', function (socket) {
   // create a new player and add it to our players object
   players[socket.id] = {
     rotation: 0,
-    x: Math.floor(Math.random() * 700) + 50,
-    y: Math.floor(Math.random() * 500) + 50,
+    x: 5600 / 2,
+    y: 5600 / 2,
     playerId: socket.id,
-    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
   };
   // send the players object to the new player
   socket.emit('currentPlayers', players);
   // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
+  socket.broadcast.emit('playerJoined', players[socket.id]);
 
   // when a player disconnects, remove them from our players object
   socket.on('disconnect', function () {
